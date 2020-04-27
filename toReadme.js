12aeda1e23cbd4b11e12594eeb2746d06ec5a7c7
Luri <luri@kioss.com>
2020-04-27 22:31:20 +0700

    setup readme generator and workflow

---
+++ toReadme.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+
+const main = async () => {
+  try {
+    const dataAsString = await fs.readFileSync(path.resolve(`./events.json`), { encoding: 'utf-8' });
+    const dataAsJson = JSON.parse(dataAsString);
+
+    var readmeHeader = "# Developer Circle Jakarta - Public Data";
+    readmeHeader += "\nDeveloper Circle Jakarta - Public Data";
+    readmeHeader += "\n\n";
+    var readmeContent = "\n";
+    var tableOfContents = "\n## Daftar Event\n"
+    tableOfContents += "\nsee [events.json](events.json)\n";
+    var count = dataAsJson.data.length;
+    dataAsJson.data.forEach(event => {
+      tableOfContents += "\n- [" + event.name + "](#)"; //todo: add hyperlink
+
+      readmeContent += "\n### " + event.name + "\n";
+      readmeContent += "\n- Tanggal: " + event.date;
+      if (event.time != ""){
+        readmeContent += "\n- Pukul: " + event.time;
+      }
+      if (event.speaker.name != ""){
+        readmeContent += "\n- Pembicara: " + event.speaker.name;
+        if (event.speaker.title != ""){
+          readmeContent += ", " + event.speaker.title;
+        }
+      }
+      if (event.url != ""){
+        readmeContent += "\n- " + event.url;
+      }
+
+      readmeContent += "\n";
+    });
+
+    readmeContent += "\n";
+
+    var readmeFooter = "\n## Bantu kami\n";
+    readmeFooter += "\nBantu kami memperbarui daftar ini 🙏\n";
+    readmeFooter += "\nPantau grup [Facebook Developer Circle Jakarta](https://www.facebook.com/groups/devcjakarta) dan silahkan perbarui data setiap kali ada sesi baru.\n";
+    readmeFooter += "\nKami sangat berterima kasih untuk setiap bantuan yang teman-teman berikan.\n";
+    readmeFooter += "\n----\n";
+    readmeFooter += "\n©️ 2020 by DevC Jakarta Team";
+
+    readmeContent = readmeHeader + tableOfContents + readmeContent + readmeFooter;
+    console.log(readmeContent);
+    fs.writeFile(path.resolve('./README.md'), readmeContent, function (err){
+      if (err){
+        return console.log('❌ Error write file README.md', err);
+      }
+      console.log('✅ Success write file README.md');
+    });
+
+  } catch (error) {
+    console.error('❌ Error read file events.json', error);
+  }
+}
+
+main();
+
